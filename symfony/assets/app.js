60d3816a14616c82617017273464264e8abf9856
Gilles <gilles@la-quincaillerie.com>
2026-03-27 17:51:01 +0100

    Add swipper

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -27,7 +27,7 @@ import './styles/_cardCity.css';
 
 /* SCRIPT JS */
 import starRating from './scripts/starRating.js';
-import swiper from './scripts/swiper.js';
+import './scripts/swiper.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     starRating();
